ca5db4ad6ef2749b3c21bc931687d5d9aa47dacb
Illya Manin <ilay.manin@gmail.com>
2024-01-27 22:47:27 -0800

    hero section added image and set up the container for text (no image crop/border radius and no page width spanning background color

--- src/Hero.js
+++ src/Hero.js
@@ -1,13 +1,20 @@
+import photo from "./images/restauranfood.jpg"
+import "./Hero.css";
+import "@fontsource/markazi-text";
+import '@fontsource/karla';
+
 export default function Hero() {
-    const the_style = {
-        backgroundColor: "#ed0dd9",
-    }
     return (
-        <section style={the_style}>
-            <h1>Little Lemon</h1>
-            <h3>Chicago</h3>
-            <p>We are a family owned Mediterranean restaurant, focused on traditional recepies with a modern twist</p>
-            <button>Reserve a Table</button>
-        </section>
+        <div className="hero_section">
+            <section className="text">
+                <section className="real_text">
+                    <h1>Little Lemon</h1>
+                    <h3>Chicago</h3>
+                    <p>We are a family owned Mediterranean restaurant, focused on traditional recepies with a modern twist</p>
+                    <button>Reserve a Table</button>
+                </section>
+            </section>
+            <img src={photo} className="photo"/>
+        </div>
     )
 }
