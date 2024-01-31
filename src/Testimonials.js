87af6131570f7c22051aba06bf0556fddd83667f
Illya Manin <ilay.manin@gmail.com>
2024-01-31 11:57:14 -0800

    nvm

--- src/Testimonials.js
+++ src/Testimonials.js
@@ -8,9 +8,9 @@ export default function Testimonials() {
     return (
         <section className="testimonial_grid">
             <div className="margin_wrapper">
-                {comments_data.map((item) => {
+                {comments_data.map((item) => (
                     <Testimonial rating={item.Rating} review={item.Review} />
-                })}
+                ))}
             </div>
         </section>
     )
