7d0379e9849109f75135257fbe99099ec5453c49
Illya Manin <ilay.manin@gmail.com>
2024-01-31 20:47:54 -0800

    footer done

--- src/Footer.js
+++ src/Footer.js
@@ -1,10 +1,35 @@
-import { renderHook } from "@testing-library/react"
+import "@fontsource/markazi-text";
+import '@fontsource/karla';
+import "./Footer.css";
+import photo from "./images/restauranfood.jpg"
 
 export default function Footer() {
-    const the_style = {
-        backgroundColor: "#e08178",
-    }
     return (
-        <footer style={the_style}>yeah</footer>
+        <footer>
+            <div className="footer_grid">
+                <img src={photo} className="footer_photo"/>
+                <ul className="footer_nav_list">
+                        <p>Doormat Navigation</p>
+                        <li>Home</li>
+                        <li>About</li>
+                        <li>Menu</li>
+                        <li>Reservations</li>
+                        <li>Order Online</li>
+                        <li>Login</li>
+                </ul>
+                <ul className="footer_contact_list">
+                        <p>Contact</p>
+                        <li>adress</li>
+                        <li>phone number</li>
+                        <li>email</li>
+                </ul>
+                <ul className="footer_socials_list">
+                        <p>Contact</p>
+                        <li>adress</li>
+                        <li>phone number</li>
+                        <li>email</li>
+                </ul>
+            </div>
+        </footer>
     )
 }
