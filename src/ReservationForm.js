9bb085155e4526c171f4ce6f0130d4b8deb085e1
Illya Manin <ilay.manin@gmail.com>
2024-02-03 14:26:56 -0800

    FORM CLEARING WORKS + PUSHED UP STATE YEAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAH

--- src/ReservationForm.js
+++ src/ReservationForm.js
@@ -3,47 +3,39 @@ import { useState, useEffect } from "react";
 import "./ReservationForm.css";
 import '@fontsource/karla';
 
-export default function ReservationForm() {
+function form_valid(props) {
+    return props.date != "" && props.time  != "" && props.guestCount > 0 && props.occasion != "";
+}
+
+export default function ReservationForm(props) {
 
     const button_style_rewrite = {
         backgroundColor: "#edefee",
     }
 
-    const [date, setDate] = useState();
-    const [availableTimes, setAvailableTimes] = useState(["17:00", "18:00", "19:00", "20:00", "21:00", "22:00"]);
-    const [time, setTime] = useState();
-    const [guestCount, setGuestCount] = useState();
-    const [occasion, setOccasion] = useState();
-
-    const form_valid = () => {
-        return (
-            date && time && guestCount > 0 && occasion
-        );
-    }
-
     const submit_handler = (e) => {
-        e.preventDefault();
-        alert("submitted");
-        form_clear();
+        alert(form_valid(props));
+        if (form_valid(props) === true) form_clear();
     }
 
     const form_clear = () => {
-        setDate("");
-        setTime("");
-        setGuestCount(0);
-        setOccasion("");
+        props.set_date("");
+        props.set_time("");
+        props.set_guestCount(0);
+        props.set_occasion("");
     }
 
     return (
         <form className="reservation_form">
             <label className="form_date">
                 Choose the date: 
-                <input type="date" onInput={e => setDate(e.target.value)}/>
+                <input defaultValue="" value={props.date} type="date" onInput={e => props.set_date(e.target.value)}/>
             </label>
             <label className="form_time">
                 <div>Choose the time: </div>
-                <select onChange={e => setTime(e.target.value)}>
-                    {availableTimes.map(item => {
+                <select defaultValue="" value={props.time} onChange={e => props.set_time(e.target.value)}>
+                    <option value="" disabled hidden/> 
+                    {props.the_times.map(item => {
                         return <option value={item}>{item}</option>
                     })}
                 </select>
@@ -54,19 +46,21 @@ export default function ReservationForm() {
                     type="number"
                     min={1}
                     max={10}
-                    onChange={e => setGuestCount(e.target.value)}
+                    value={props.guestCount}
+                    onChange={e => props.set_guestCount(e.target.value)}
                 />
             </label>
             <label className="form_occasion">
                 Occasion: 
-                <select onChange={e => setOccasion(e.target.value)}>
+                <select defaultValue="" value={props.occasion} onChange={e => props.set_occasion(e.target.value)}>
+                    <option value="" disabled hidden/> 
                     <option value="Birthday">Birthday</option>    
                     <option value="Anniversary">Anniversary</option>    
                     <option value="N/A">N/A</option>    
                 </select>
             </label>
             <div className="hey_why_is_react_not_making_a_wrapper">
-            <Button type="button" text="submit" customClickEvent={(form_valid) ? submit_handler : ""} />
+            <Button type="button" text="submit" customClickEvent={submit_handler} />
             </div>
         </form>
     )
--- src/Reservations.js
+++ src/Reservations.js
@@ -1,12 +1,19 @@
 import ReservationForm from "./ReservationForm"
 import "./Reservations.css";
 import background_photo from "./images/restaurant.jpg"
+import {useState, useReducer} from "react";
 
 export default function Reservations() {
 
+    const [availableTimes, setAvailableTimes] = useState(["17:00", "18:00", "19:00", "20:00", "21:00", "22:00"]);
+    const [date, setDate] = useState(undefined);
+    const [time, setTime] = useState(undefined);
+    const [guestCount, setGuestCount] = useState(0);
+    const [occasion, setOccasion] = useState(undefined);
+
     return (
         <div className="reservation_page">
-            <ReservationForm className="the_form_wrapper"/>
+            <ReservationForm occasion={occasion} set_occasion={setOccasion} guestCount={guestCount} set_guestCount={setGuestCount} time={time} set_time={setTime} date={date} set_date={setDate} the_times={availableTimes} className="the_form_wrapper"/>
         </div>
     )
 }
