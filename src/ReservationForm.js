ead609c64e5f2825830715bcf55b1a15b89d61b6
Illya Manin <ilay.manin@gmail.com>
2024-02-04 09:21:35 -0800

    aria labels

--- src/ReservationForm.js
+++ src/ReservationForm.js
@@ -25,11 +25,11 @@ export default function ReservationForm(props) {
         <form className="reservation_form">
             <label className="form_date">
                 Choose the date:
-                <input defaultValue="" value={props.date} type="date" onInput={e => props.set_date(e.target.value)}/>
+                <input name="date_input" defaultValue="" value={props.date} type="date" onInput={e => props.set_date(e.target.value)}/>
             </label>
             <label className="form_time">
                 <div>Choose the time: </div>
-                <select defaultValue="" value={props.time} onChange={e => props.set_time(e.target.value)}>
+                <select name="time_input" defaultValue="" value={props.time} onChange={e => props.set_time(e.target.value)}>
                     <option value="" disabled hidden/> 
                     {props.the_times.map(item => {
                         return <option value={item}>{item}</option>
@@ -40,6 +40,7 @@ export default function ReservationForm(props) {
                 Number of Guests: 
                 <input 
                     type="number"
+                    name="guest_amount"
                     min={1}
                     max={10}
                     value={props.guestCount}
@@ -48,7 +49,7 @@ export default function ReservationForm(props) {
             </label>
             <label className="form_occasion">
                 Occasion: 
-                <select defaultValue="" value={props.occasion} onChange={e => props.set_occasion(e.target.value)}>
+                <select name="occasion" defaultValue="" value={props.occasion} onChange={e => props.set_occasion(e.target.value)}>
                     <option value="" disabled hidden/> 
                     <option value="Birthday">Birthday</option>    
                     <option value="Anniversary">Anniversary</option>    
@@ -56,8 +57,9 @@ export default function ReservationForm(props) {
                 </select>
             </label>
             <div className="hey_why_is_react_not_making_a_wrapper">
-            {(form_valid(props) === true) ? <Link to="/reservation-confirmation" className="no_deco_link where_is_padding"><Button type="button" text="submit" customClickEvent={submit_handler} /></Link> : <Button 
+            {(form_valid(props) === true) ? <Link to="/reservation-confirmation" className="no_deco_link where_is_padding"><Button aria-label="submit" type="button" text="submit" customClickEvent={submit_handler} /></Link> : <Button 
             type="button" 
+            aria-label="submit"
             text="submit" 
             disabled_button="disabled"/> }
             </div>
